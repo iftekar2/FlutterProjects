1f42486ccd3f8e2c5dd7918f8f19d5298b5459d1
Iftekar Hossain <iftekar.hossain123@gmail.com>
2025-06-22 22:08:47 -0400

    Fixing Box Size

--- sneaker_app/lib/components/shoe_tile.dart
+++ sneaker_app/lib/components/shoe_tile.dart
@@ -8,29 +8,37 @@ class ShoeTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(right: 20),
-      width: 300,
-      height: 400,
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+      child: Container(
+        margin: EdgeInsets.only(right: 20),
+        width: 300,
+        //padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+        ),
 
-      child: Column(
-        children: [
-          // Shoe image
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.asset(shoe.image),
-          ),
-          // Shoe description
+        child: Column(
+          children: [
+            // Shoe image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(shoe.image),
+            ),
+            // Shoe description
+            Padding(
+              padding: const EdgeInsets.only(top: 10, left: 10),
+              child: Text(
+                shoe.name,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+            ),
+            // Shoe price
 
-          // Shoe price
-
-          // Add to bag button
-        ],
+            // Add to bag button
+          ],
+        ),
       ),
     );
   }
