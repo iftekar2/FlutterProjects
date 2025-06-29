7052d6f421c22476a5b000d636750c4833ffbfe8
Iftekar Hossain <iftekar.hossain123@gmail.com>
2025-06-29 17:25:07 -0400

    Pushing in Code

--- shoping_app/lib/pages/intro_page.dart
+++ shoping_app/lib/pages/intro_page.dart
@@ -104,11 +104,26 @@ class IntroPage extends StatelessWidget {
                 ),
               ),
 
-              // Manual Login
-
-              // Register Page Link
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(color: Colors.grey[300], thickness: 1),
+                    ),
+                    Text(
+                      "Or log in",
+                      style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+                    ),
+                    Expanded(
+                      child: Divider(color: Colors.grey[300], thickness: 1),
+                    ),
+                  ],
+                ),
+              ),
 
               // Continue
+              Text("Already have an account "),
             ],
           ),
         ),
