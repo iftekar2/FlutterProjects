83840e7c51e8cb415feeb63d90e1eb4bdd86e6a2
Iftekar Hossain <iftekar.hossain123@gmail.com>
2025-06-29 19:32:41 -0400

    Setting all page background color to be white

--- shoping_app/lib/main.dart
+++ shoping_app/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       debugShowCheckedModeBanner: false,
       home: IntroPage(homePage: const HomePage()),
     );
