85a9d6eace96d0112c36612c47da592d3fbcbcde
Iftekar Hossain <iftekar.hossain123@gmail.com>
2025-06-30 22:18:16 -0400

    Drawer bar done

--- shoping_app/lib/pages/home_page.dart
+++ shoping_app/lib/pages/home_page.dart
@@ -7,12 +7,13 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         leading: Padding(
           padding: const EdgeInsets.only(left: 10, right: 10),
           child: Builder(
             builder: (context) {
               return IconButton(
-                icon: Icon(Icons.menu),
+                icon: Icon(Icons.menu, size: 30),
                 onPressed: () => {Scaffold.of(context).openDrawer()},
               );
             },
@@ -30,6 +31,101 @@ class HomePage extends StatelessWidget {
                 Icons.shopping_cart_outlined,
                 size: 80,
                 color: Colors.grey[500],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.home, size: 30),
+                      SizedBox(width: 10),
+                      Text(
+                        'Home',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.shopping_cart, size: 30),
+                      SizedBox(width: 10),
+                      Text(
+                        "Cart",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.settings, size: 30),
+                      SizedBox(width: 10),
+                      Text(
+                        "Settings",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.info_outline, size: 30),
+                      SizedBox(width: 10),
+                      Text(
+                        "About",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.exit_to_app, size: 30),
+                  SizedBox(width: 10),
+                  Text(
+                    "Exit",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
