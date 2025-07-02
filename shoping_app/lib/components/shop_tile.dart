999f565163b429eeaff337b5d25519ea36c1cc76
Iftekar Hossain <iftekar.hossain123@gmail.com>
2025-07-01 22:38:58 -0400

    Created ShoppingItems Class

---
+++ shoping_app/lib/components/shop_tile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Shop extends StatelessWidget {
+  const Shop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+        
+      ],
+    );
+  }
+}
---
+++ shoping_app/lib/model/shopping_items.dart
@@ -0,0 +1,13 @@
+class ShoppingItems {
+  final String name;
+  final String price;
+  final String description;
+  final String image;
+
+  ShoppingItems({
+    required this.name,
+    required this.price,
+    required this.description,
+    required this.image,
+  });
+}
