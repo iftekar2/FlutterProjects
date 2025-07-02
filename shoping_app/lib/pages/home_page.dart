3f83720e2c3e4ad8c23c7b9760bea85990f38d29
Iftekar Hossain <iftekar.hossain123@gmail.com>
2025-07-02 07:43:10 -0400

    Making changes

--- shoping_app/lib/pages/home_page.dart
+++ shoping_app/lib/pages/home_page.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:shoping_app/components/app_drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // final List<Widget> _pages = [const ShopPage(), const CartPage()];
+
+  int _currentIndex = 0;
+  void navigation(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,28 +23,140 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         leading: Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10),
+          padding: const EdgeInsets.only(left: 0, right: 0),
           child: Builder(
             builder: (context) {
               return IconButton(
-                icon: const Icon(Icons.menu, size: 30),
-                onPressed: () => Scaffold.of(context).openDrawer(),
+                icon: Icon(Icons.menu, size: 30),
+                onPressed: () => {Scaffold.of(context).openDrawer()},
               );
             },
           ),
         ),
+
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 10),
             child: IconButton(
               onPressed: () {},
-              icon: const Icon(Icons.shopping_cart, size: 30),
+              icon: Icon(Icons.shopping_cart, size: 30),
             ),
           ),
         ],
       ),
-      drawer: const AppDrawer(), // Using the extracted AppDrawer widget
-      body: const Center(child: Text('Welcome to the Shopping App!')),
+
+      drawer: Drawer(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 300,
+              child: Icon(
+                Icons.shopping_cart_outlined,
+                size: 80,
+                color: Colors.grey[500],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.home, size: 30),
+                      SizedBox(width: 10),
+                      Text(
+                        'Home',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.shopping_cart, size: 30),
+                      SizedBox(width: 10),
+                      Text(
+                        "Cart",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.settings, size: 30),
+                      SizedBox(width: 10),
+                      Text(
+                        "Settings",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.info_outline, size: 30),
+                      SizedBox(width: 10),
+                      Text(
+                        "About",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.exit_to_app, size: 30),
+                  SizedBox(width: 10),
+                  Text(
+                    "Exit",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      //body: _pages,
     );
   }
 }
