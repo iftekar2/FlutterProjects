8e303b55c3acc4452e157f3165c7c6fa4ff861b3
Iftekar Hossain <iftekar.hossain123@gmail.com>
2025-07-04 23:53:01 -0400

    Final commit for today

--- shoping_app/lib/pages/cart_page.dart
+++ shoping_app/lib/pages/cart_page.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:shoping_app/model/shop.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
-  const CartPage({super.key});
+  CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Get access to the cart
+    final cart = context.watch<Shop>().getCart();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: const Text("Cart Page"),
+      ),
+
+      body: Column(
+        children: [
+          // Items in the cart
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                // Show items in the cart
+                final item = cart[index];
+
+                // Return the cart tile
+                return ListTile(
+                  title: Text(item.name),
+                  trailing: Text(item.price.toString()),
+                  trailing: 
+                );
+              },
+            ),
+          ),
+
+          // Pay
+        ],
       ),
     );
   }
