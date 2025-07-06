ce23d7fcf892bed5e0b7eedef435e63b05995953
Iftekar Hossain <iftekar.hossain123@gmail.com>
2025-07-05 23:40:37 -0400

    Create account page is done

--- authentication/lib/pages/email_signup_page.dart
+++ authentication/lib/pages/email_signup_page.dart
@@ -6,8 +6,117 @@ class EmailSignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Email Signup')),
-      body: const Center(child: Text('Email Signup Page')),
+      backgroundColor: Colors.grey[300],
+      body: Padding(
+        padding: const EdgeInsets.only(left: 40, right: 40),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Page title
+              Text(
+                "Create an account",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+              ),
+              SizedBox(height: 50),
+
+              Column(
+                children: [
+                  // Full name field
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Full Name",
+                      hintStyle: TextStyle(fontSize: 20),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 12,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+                  // Email field
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Email",
+                      hintStyle: TextStyle(fontSize: 20),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 12,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+                  // Password field
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      hintStyle: TextStyle(fontSize: 20),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 12,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+                  // Confirm password field
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Confirm Password",
+                      hintStyle: TextStyle(fontSize: 20),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 12,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 80),
+              // Create account button
+              SizedBox(
+                height: 60,
+                width: 500,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    elevation: 0,
+                    // This is to remove the ripple effect when button is pressed
+                    shadowColor: Colors.transparent,
+                    splashFactory: NoSplash.splashFactory,
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(width: 10),
+                      Text(
+                        "Create Account",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
