963327da16e3da6e611717e9810c48a448dd1b83
Iftekar Hossain <iftekar.hossain123@gmail.com>
2025-07-23 22:42:48 -0400

    Users are able to reset password

--- blog_app/lib/pages/forgot_password_page.dart
+++ blog_app/lib/pages/forgot_password_page.dart
@@ -1,7 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ForgotPasswordPage extends StatelessWidget {
+class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({super.key});
+
+  @override
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  String email = '';
+
+  TextEditingController emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  resetPassword() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.orange,
+          content: Text(
+            "Password reset email sent",
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Colors.orange,
+            content: Text(
+              "No user found with this email",
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,78 +50,90 @@ class ForgotPasswordPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.only(left: 20, right: 20, top: 200),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Reset Password",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 10),
-              Text(
-                "Please enter your email blow",
-                style: TextStyle(fontSize: 18),
-              ),
-
-              SizedBox(height: 25),
-              Text("Email", style: TextStyle(fontSize: 20)),
-
-              SizedBox(height: 10),
-              TextFormField(
-                style: TextStyle(fontSize: 20),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide(color: Colors.black, width: 1.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide(color: Colors.black, width: 1.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide(color: Colors.black, width: 2.0),
-                  ),
-
-                  hint: Text(
-                    "Enter your email",
-                    style: TextStyle(fontSize: 20, color: Colors.grey[600]),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 30),
-              SizedBox(
-                height: 60,
-                width: 400,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    elevation: 0,
-                    shadowColor: Colors.transparent,
-                    splashFactory: NoSplash.splashFactory,
-                  ),
-                  onPressed: () {},
-                  child: const Text(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
                     "Reset Password",
                     style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
+                      color: Colors.black,
+                      fontSize: 25,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-              ),
-            ],
+
+                SizedBox(height: 10),
+                Text(
+                  "Please enter your email blow",
+                  style: TextStyle(fontSize: 18),
+                ),
+
+                SizedBox(height: 25),
+                Text("Email", style: TextStyle(fontSize: 20)),
+
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: emailController,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(color: Colors.black, width: 1.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(color: Colors.black, width: 1.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    ),
+
+                    hint: Text(
+                      "Enter your email",
+                      style: TextStyle(fontSize: 20, color: Colors.grey[600]),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 30),
+                SizedBox(
+                  height: 60,
+                  width: 400,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      elevation: 0,
+                      shadowColor: Colors.transparent,
+                      splashFactory: NoSplash.splashFactory,
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        setState(() {
+                          email = emailController.text;
+                        });
+                        resetPassword();
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: const Text(
+                      "Reset Password",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
